3cbf954b2a9ad7c72353a54219ec7ee9e790566a
Hugo Hernandez <hugo20016@gmail.com>
2021-10-02 18:46:37 +0200

    Commit guardarCSV y Datos

--- Practica1_DIS.java
+++ Practica1_DIS.java
@@ -1,3 +1,26 @@
-public static void main(String[]args){
+
+// Hugo Hernandez - metodo guardar en CSV
+private static void guardarCSV(Datos[] datos_csv) throws IOException {
+        String[] valores = new String[18025];
+        int contador = 0;
+        String archCSV = ".\\salida.csv";
+        CSVWriter writer = new CSVWriter(new FileWriter(archCSV));
+
+        for(int i = 0; i < datos_csv.length; i++){
+        if("Credit".equals(datos_csv[i].getSeries_title_2())){
+        valores[contador] = String.valueOf(datos_csv[i].getData_value());
+        contador++;
+        }
 
         }
+
+        writer.writeNext(valores);
+        writer.close();
+
+        System.out.println("Archivo .csv generado");
+        }
+
+public static void main(String[]args){
+
+}
+
